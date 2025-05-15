52b6b2998a2766c9c2528573b147b038f5cefb0e
Hussain Khokhar <hussainkhokhar800@gmail.com>
2025-05-15 18:44:41 +0500

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from "./Header"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+     
+    </>
+  )
+}
+
+export default App;
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,10 @@
+function  Header(params) {
+           return(
+                <header className="header">
+      <img src="https://static.vecteezy.com/system/resources/previews/021/769/089/original/chef-logo-free-vector.jpg" alt="Chef Claude" style={{ width: '80px', height: 'auto' }} />
+
+      <h1>Chef Claude</h1>
+    </header>
+           )
+}
+export default Header;
